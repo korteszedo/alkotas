90d5bc2d911b4fbb7f3d9f0516f70c94804e4355
SigmaSpeedy9 <tothbalint249@gmail.com>
2025-04-15 09:37:29 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -19,6 +19,38 @@ const tableDiv = makeDiv('table');
 // Létrehoz egy "form" osztályú <div> elemet
 const formDiv = makeDiv('form');
 
+// Létrehoz egy <table> elemet
+const tableSim = document.createElement('table');
+// Hozzáadja a <table> elemet a "table" osztályú <div>-hez
+tableDiv.appendChild(tableSim);
+
+// Létrehoz egy <thead> elemet
+const tableHead = document.createElement('thead');
+// Hozzáadja a <thead> elemet a <table>-hez
+tableSim.appendChild(tableHead);
+
+// Létrehoz egy <tr> elemet a táblázat fejléce számára
+const tableHeadRow = document.createElement('tr');
+// Hozzáadja a <tr> elemet a <thead>-hez
+tableHead.appendChild(tableHeadRow);
+
+// A táblázat fejléceinek szövegeit tartalmazó tömb
+const theadCells = ['Szerző', 'Műfaj', 'Cím'];
+// Végigmegy a fejlécek szövegein
+for (const cellContent of theadCells) {
+    // Létrehoz egy <th> elemet
+    const thcell = document.createElement('th');
+    // Beállítja a <th> elem szövegét
+    thcell.innerText = cellContent;
+    // Hozzáadja a <th> elemet a fejléc sorához
+    tableHeadRow.appendChild(thcell);
+}
+
+// Létrehoz egy <tbody> elemet
+const tbody = document.createElement('tbody');
+// Hozzáadja a <tbody> elemet a <table>-hez
+tableSim.appendChild(tbody);
+
 // A "table" elemet hozzáadja a "container" elemhez
 containerDiv.appendChild(tableDiv);
 // A "form" elemet is hozzáadja a "container" elemhez
